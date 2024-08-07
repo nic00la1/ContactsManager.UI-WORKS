bbbb63cf4a418054c0536295373b722af5121d88
nic00la <nic00la.kaleta@gmail.com>
2024-08-07 17:10:29 +0200

    Register View ✨

---
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager.Core.DTO;
+
+public class RegisterDTO
+{
+    [Required] public string PersonName { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage =
+        "E-mail should be in a proper email address format")]
+    public string Email { get; set; }
+
+    [Required]
+    [RegularExpression("^[0-9]*$",
+        ErrorMessage = "Phone number should contain numbers only")]
+    public string Phone { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
