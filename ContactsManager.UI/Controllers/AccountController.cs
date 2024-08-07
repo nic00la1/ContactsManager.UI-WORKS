657dd33f694c6314f26246fb666df7f54448569f
nic00la <nic00la.kaleta@gmail.com>
2024-08-07 17:51:27 +0200

    User Manager ✅✨

--- ContactsManager.UI/Controllers/AccountController.cs
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using ContactsManager.Core.DTO;
+﻿using ContactsManager.Core.Domain.IdentityEntities;
+using ContactsManager.Core.DTO;
 using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Controllers;
@@ -7,6 +9,13 @@ namespace ContactsManager.UI.Controllers;
 [Route("[controller]/[action]")]
 public class AccountController : Controller
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
     [HttpGet]
     public IActionResult Register()
     {
@@ -14,9 +23,33 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public IActionResult Register(RegisterDTO registerDto)
+    public async Task<IActionResult> Register(RegisterDTO registerDto)
     {
-        // TODO: Store user registration details into Identity database
-        return RedirectToAction(nameof(PersonsController.Index), "Persons");
+        // Check for validation errors
+        if (ModelState.IsValid == false)
+        {
+            ViewBag.Errors = ModelState.Values.SelectMany(e => e.Errors)
+                .Select(t => t.ErrorMessage);
+            return View(registerDto);
+        }
+
+        ApplicationUser user = new()
+        {
+            Email = registerDto.Email,
+            PhoneNumber = registerDto.Phone,
+            UserName = registerDto.Email,
+            PersonName = registerDto.PersonName
+        };
+
+        IdentityResult result =
+            await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (result.Succeeded)
+            return RedirectToAction(nameof(PersonsController.Index), "Persons");
+        else
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("Register", error.Description);
+
+        return View(registerDto);
     }
 }
